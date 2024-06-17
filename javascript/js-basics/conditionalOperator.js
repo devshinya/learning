f5bce1d0e291848d97453c715e21392ecddf19e4
shinya <shinya.matsunaga11+dev@gmail.com>
2024-06-17 21:18:10 +0900

    add: conditionalOperator.js

---
+++ javascript/js-basics/conditionalOperator.js
@@ -0,0 +1,9 @@
+// ## 三項演算子
+
+// 三項演算子は、条件式を評価して、その結果によって値を返す演算子です。
+// 三項演算子は、次のように記述します。
+// 条件式 ? trueの場合の値 : falseの場合の値
+// 例)
+const age = 20;
+const message = age >= 20 ? "Adult" : "Child";
+console.log(message); // => Adult
