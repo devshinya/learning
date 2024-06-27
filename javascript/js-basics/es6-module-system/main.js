c2e3219a2a19d65bb07e45d447fed6a6fa60c3d1
shinya <shinya.matsunaga11+dev@gmail.com>
2024-06-27 23:18:58 +0900

    add: ESModule source

---
+++ javascript/js-basics/es6-module-system/main.js
@@ -0,0 +1,5 @@
+import multiply, { add, subtract as sub } from "./module.js";
+
+console.log(add(5, 3)); // 8
+console.log(sub(5, 3)); // 2
+console.log(multiply(5, 3)); // 15
---
+++ javascript/js-basics/es6-module-system/module.js
@@ -0,0 +1,13 @@
+// 名前付きエクスポート
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+// デフォルトエクスポート
+export default function multiply(a, b) {
+  return a * b;
+}
